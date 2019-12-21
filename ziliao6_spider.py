3f3ebc2d432fc0c119f69fdbd766871020ade4f2
Annihilater <yanmiexingkong@gmail.com>
2019-12-21 16:35:05 +0800

    chore: debug file for pycharm

---
+++ ziliao6_spider.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date  : 2019/12/21 8:54 上午
+# @Author: yanmiexingkong
+# @email : yanmiexingkong@gmail.com
+# @File  : ziliao6_spider.py
+from scrapy import cmdline
+
+
+cmdline.execute('scrapy crawl ziliao6'.split())
+
